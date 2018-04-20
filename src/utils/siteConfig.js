78d9d36d15a4c66a91d4c7d6bbb71a3f0fc13099
Sam Uherek <samuherekbiz@gmail.com>
2018-04-20 21:45:36 +0200

    initial commit for the blog

---
+++ src/utils/siteConfig.js
@@ -0,0 +1,5 @@
+module.exports = {
+  siteTitle: 'Your Fair Advantage',
+  siteDescription: 'A place where you get to know yourselfe a bit more than usual.',
+  siteUrl: 'https://yourfairadvantage.com' // Site domain. Do not inculde a trailing slash! If you wish to use a path prefix you can read more about that here: https://www.gatsbyjs.org/docs/path-prefix/
+};
