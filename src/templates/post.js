781d91ed50091bad38fa8baeae5d8832c5eb866f
Sam Uherek <samuherekbiz@gmail.com>
2018-04-21 07:34:04 +0200

    getting ready for netlify

--- src/templates/post.js
+++ src/templates/post.js
@@ -1,36 +1,22 @@
-import React from 'react'
-import find from 'lodash/find'
-import Helmet from 'react-helmet'
-import styled from 'styled-components'
-import config from '../utils/siteConfig'
-import Hero from '../components/Hero'
-import Container from '../components/Container'
-import PageBody from '../components/PageBody'
-import TagList from '../components/TagList'
-import PostLinks from '../components/PostLinks'
-import PostDate from '../components/PostDate'
+import React from 'react';
+import find from 'lodash/find';
+import Helmet from 'react-helmet';
+import styled from 'styled-components';
+import config from '../utils/siteConfig';
+import Hero from '../components/Hero';
+import Container from '../components/Container';
+import PageBody from '../components/PageBody';
+import TagList from '../components/TagList';
+import PostLinks from '../components/PostLinks';
+import PostDate from '../components/PostDate';
 
-const PostTemplate = ({data}) => {
+const PostTemplate = ({ data }) => {
+  const { title, slug, id, heroImage, description, body, publishDate, tags } = data.contentfulPost;
 
-  const {
-    title,
-    slug,
-    id,
-    heroImage,
-    description,
-    body,
-    publishDate,
-    tags,
-  } = data.contentfulPost;
+  const postIndex = find(data.allContentfulPost.edges, ({ node: post }) => post.id === id);
 
-  const postIndex = find(
-    data.allContentfulPost.edges,
-    ({ node: post }) => post.id === id
-  );
-
-  return(
+  return (
     <div>
-
       <Helmet>
         <title>{`${title} - ${config.siteTitle}`}</title>
         <meta property="og:title" content={`${title} - ${config.siteTitle}`} />
@@ -38,26 +24,21 @@ const PostTemplate = ({data}) => {
         <meta property="og:image" content={heroImage.sizes.src} />
       </Helmet>
 
-      <Hero
-        title={title}
-        image={heroImage}
-        height={'50vh'}
-      />
+      <Hero title={title} image={heroImage} height={'50vh'} />
 
       <Container>
-        {tags && (<TagList tags={tags} />)}
-        <PostDate date={publishDate}/>
+        {tags && <TagList tags={tags} />}
+        <PostDate date={publishDate} />
         <PageBody body={body} />
         <PostLinks previous={postIndex.previous} next={postIndex.next} />
       </Container>
-
     </div>
-  )
-}
+  );
+};
 
 export const query = graphql`
   query postQuery($slug: String!) {
-    contentfulPost(slug: {eq: $slug}) {
+    contentfulPost(slug: { eq: $slug }) {
       title
       id
       slug
@@ -79,7 +60,7 @@ export const query = graphql`
         }
       }
     }
-    allContentfulPost(limit: 1000, sort: { fields: [publishDate], order: DESC })  {
+    allContentfulPost(limit: 1000, sort: { fields: [publishDate], order: DESC }) {
       edges {
         node {
           id
@@ -93,6 +74,6 @@ export const query = graphql`
       }
     }
   }
-`
+`;
 
-export default PostTemplate
+export default PostTemplate;
